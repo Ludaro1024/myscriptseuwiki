64dcf1eae55a4934449431349bbd0a90d721a671
Ludaro <waschmaschvanlu@gmail.com>
2024-09-12 06:40:20 +0200

    added some things

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,27 +62,34 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // navbar: {
-      //   title: 'My Site',
-      //   logo: {
-      //     alt: 'My Site Logo',
-      //     src: 'img/logo.svg',
-      //   },
-      //   items: [
-      //     {
-      //       type: 'doc',
-      //       docId: 'intro',
-      //       position: 'left',
-      //       label: 'Tutorial',
-      //     },
-      //     {to: '/blog', label: 'Blog', position: 'left'},
-      //     {
-      //       href: 'https://github.com/facebook/docusaurus',
-      //       label: 'GitHub',
-      //       position: 'right',
-      //     },
-      //   ],
-      // },
+      navbar: {
+        title: 'MyScripts Wiki',
+        logo: {
+          alt: 'Wiki',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            href: 'https://github.com/Ludaro1024/ludaro-wiki-ger/blob/main/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      {
+        type: 'localeDropdown',
+        position: 'right',
+        dropdownItemsAfter: [
+          {
+            type: 'html',
+            value: '<hr style="margin: 0.3rem 0;">',
+          },
+          {
+            href: 'https://github.com/Ludaro1024/ludaro-wiki-ger/i18n/',
+            label: 'Help me Translate!',
+          },
+          ],
+        },
       defaultMode: 'dark',
       disableSwitch: false,
       respectPrefersColorScheme: false,
@@ -95,6 +102,60 @@ const config = {
         darkTheme: lightCodeTheme,
       },
     }),
+    footer: {
+      style: 'dark',
+      links: [
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     {
+        //       label: 'Tutorial',
+        //       to: '/docs/intro',
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: 'More',
+        //   items: [
+        //     {
+        //       label: 'Blog',
+        //       to: '/blog',
+        //     },
+        //     {
+        //       label: 'GitHub',
+        //       href: 'https://github.com/facebook/docusaurus',
+        //     },
+        //   ],
+        // },
+        {
+          title: 'Important Links:',
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.gg/N4Br9HDy",
+            },
+            {
+              label: "Shop"
+              href: "https://myscripts.eu"
+            }
+           
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Myscripts logo',
+        src: 'img/logo.png',
+        className: 'footer-logo',
+        href: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        width: 100,
+        height: 100,
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} MyScripts, written with ❤️ for you with Docusaurus`,
+    },
+    prism: {
+      additionalLanguages: ['powershell', 'lua', 'php', 'csharp', 'css', 'javascript', 'sql', 'json', 'git', 'diff'],
+      theme: darkCodeTheme
+    },
 };
 
 module.exports = config;
